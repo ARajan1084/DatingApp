1169c17baf0d7ac75c63a72e9f462ef3f88f79c3
Akanksha Kumar <akkanksha.kumar@gmail.com>
2019-05-30 20:06:39 -0700

    more javadocs

--- DatingApp/src/datingapp/program/Message.java
+++ DatingApp/src/datingapp/program/Message.java
@@ -1,25 +1,50 @@
 package datingapp.program;
 
+/**
+ * constructs a Message object for the messaging system
+ */
 public class Message {
     private String message;
     private Person to;
     private Person from;
 
-    public Message (String message, Person recipient, Person sender) {
+    /**
+     * constructs a Message object
+     * @param message the message's text
+     * @param recipient the message's recipient
+     * @param sender the message's sender
+     */
+    public Message (String message, Person recipient, Person sender)
+    {
         to = recipient;
         from = sender;
         this.message = message;
     }
 
-    public String getMessage() {
+    /**
+     * gets the message
+     * @return the message
+     */
+    public String getMessage()
+    {
         return message;
     }
 
-    public Person getRecipient () {
+    /**
+     * get's the recipient of the message
+     * @return the message's recipient
+     */
+    public Person getRecipient()
+    {
         return to;
     }
 
-    public Person getSender() {
+    /**
+     * gets the sender of the message
+     * @return the message's sender
+     */
+    public Person getSender()
+    {
         return from;
     }
 }
